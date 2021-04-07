0cdd66db22c03e301d75e99dd27ed85717792a28
Anugrah D <agarangga55@gmail.com>
2021-04-08 05:41:55 +0700

    masukin SFX button

--- KetupatWars.IO Networking (2019.4.22f1)/Assets/Scripts/MenuController.cs
+++ KetupatWars.IO Networking (2019.4.22f1)/Assets/Scripts/MenuController.cs
@@ -18,6 +18,8 @@ public class MenuController : MonoBehaviour
 
     [SerializeField] private GameObject StartButton;
     [SerializeField] private GameObject MenuButtons;
+
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings(VersName);
@@ -52,54 +54,46 @@ public class MenuController : MonoBehaviour
         UsernameMenu.SetActive(false);
         PhotonNetwork.playerName = UsernameInput.text;
     }
-
     public void CreateGame()
     {
         PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions() { maxPlayers = 20 }, null);
     }
-
     public void JoinGame()
     {
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.maxPlayers = 20;
         PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, roomOptions, TypedLobby.Default);
     }
-
     private void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("Gameplay");
     }
 
+   
     public void CreateJoinSelection()
     {
         RoomPanel.SetActive(true);
     }
-
     public void CreateJoinClose()
     {
         RoomPanel.SetActive(false);
     }
-
     public void SettingMenuOpen()
     {
         SettingPanel.SetActive(true);
     }
-
     public void SettingMenuClose()
     {
         SettingPanel.SetActive(false);
     }
-    
     public void MainMenuOpen()
     {
         MenuButtons.SetActive(true);
     }
-
     public void ExitMenuOpen()
     {
         ExitPanel.SetActive(true);
     }   
-
     public void ExitMenuClose()
     {
         ExitPanel.SetActive(false);
