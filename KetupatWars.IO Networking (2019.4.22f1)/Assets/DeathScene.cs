6ccdd6139e9dfda52cf418458cc03d24ddafb3fe
Anugrah D <agarangga55@gmail.com>
2021-06-15 22:59:13 +0700

    Death Scene Exit Button

--- KetupatWars.IO Networking (2019.4.22f1)/Assets/DeathScene.cs
+++ KetupatWars.IO Networking (2019.4.22f1)/Assets/DeathScene.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class DeathScene : MonoBehaviour
 {
-    [SerializeField]private GameObject DeadInstance;
+    [SerializeField] private GameObject DeadInstance;
     public PlayerScript playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void DeathS()
     {
         Debug.Log("wanjay");
-       DeadInstance.SetActive(true);
+        DeadInstance.SetActive(true);
     }
 
     public void SetManager(PlayerScript _playerManager)
     {
-       playerController  = _playerManager;
+        playerController = _playerManager;
+    }
+
+    public void ExitRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LoadLevel("MainMenu");
+
     }
 }
